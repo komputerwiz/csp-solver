78a5f2461537862e7f7e7454dcfa0d67ade490a4
Matthew Barry <komputerwiz.matt@gmail.com>
2014-08-19 00:54:08 -0500

    Added Mathdoku CSP type
    * Square n-by-n size set in constructor
    * Battleship-style symbol variables (e.g. :A1, :C2, :B3, :D4)
    * Initial domains are 1 through n (the size of the board)
    * Cells in each row must be unique
    * Cells in each column must be unique
    * Helper methods for adding sum, difference, product, and quotient constraints

---
+++ lib/mathdoku.rb
@@ -0,0 +1,47 @@
+require 'csp'
+
+class Mathdoku < CSP
+  def initialize(n)
+    super()
+
+    cols = ('A'..('A'.ord+n-1).chr).to_a
+    rows = (1..n).to_a
+
+    vars cols.product(rows).map(&:join).map(&:to_sym), 1..6
+
+    cols.each do |c|
+      all_different(rows.map {|r| "#{c}#{r}".to_sym})
+    end
+
+    rows.each do |r|
+      all_different(cols.map {|c| "#{c}#{r}".to_sym})
+    end
+  end
+
+  def sum(value, *vars)
+    constrain(*vars) { |*args| args.inject(:+) == value }
+  end
+
+  def difference(value, *vars)
+    raise ArgumentError, 'Only two vars are allowed' unless vars.size == 2
+    constrain(*vars) { |a,b| a-b == value or b-a == value }
+  end
+
+  def product(value, *vars)
+    constrain(*vars) { |*args| args.inject(:*) == value }
+  end
+
+  def quotient(value, *vars)
+    raise ArgumentError, 'Only two vars are allowed' unless vars.size == 2
+    constrain(*vars) { |a,b| a.fdiv(b) == value or b.fdiv(a) == value }
+  end
+
+  def print!(solution)
+    rows.each do |r|
+      cols.each do |c|
+        print solution["#{c}#{r}".to_sym] + ' '
+      end
+      print "\n"
+    end
+  end
+end
